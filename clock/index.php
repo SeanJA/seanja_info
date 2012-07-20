35a5ad4eb64b6042c75180982e7f29b40db8e4cb
SeanJA <sean.sandy@gmail.com>
2012-07-19 21:46:36 -0300

    Added clock

---
+++ clock/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <script src="scripts/jquery-1.7.2.min.js"></script>
+        <script src="scripts/script.js"></script>
+        <link rel="stylesheet" href="css/style.css" type="text/css">
+    </head>
+    <body>
+        <div id="clock">
+            <span id="e0" class="pin">●</span>
+            <span id="e1" class="pin">●</span>
+            <span id="e2" class="pin">‌●</span>
+            <span id="e3" class="pin">‌●</span>
+            <div id="letters">
+            </div>
+        </div>​
+        <small>Shamelessly stolen and modified from <a href="http://codepen.io/FWeinb/pen/oyACz">here</a>.</small>
+    </body>
+</html>
