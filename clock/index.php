52f32ccd19e022b3d05eddca7166e45332fd166c
SeanJA <sean.sandy@gmail.com>
2012-07-19 22:04:33 -0300

    Added credit link for the clock

--- clock/index.php
+++ clock/index.php
@@ -5,7 +5,7 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <script src="scripts/jquery-1.7.2.min.js"></script>
         <script src="scripts/script.js"></script>
-        <link rel="stylesheet" href="css/style.css" type="text/css">
+        <link rel="stylesheet" href="css/style.css?1" type="text/css">
     </head>
     <body>
         <div id="clock">
@@ -16,6 +16,9 @@
             <div id="letters">
             </div>
         </div>​
+
+
+
         <small>Shamelessly stolen and modified from <a href="http://codepen.io/FWeinb/pen/oyACz">here</a>.</small>
     </body>
 </html>
